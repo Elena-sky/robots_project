2844985c75f7723317047f5939ab44c471836667
MacBook <alonasoroka1993@gmail.com>
2018-04-18 15:26:07 +0300

    loader

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,14 @@
 <?php
 require_once ('./php/loader/Loader.php');
+Loader::load('app.php');
 
+
+$result=[];
+if(array_key_exists('url', $_POST)){
+    $robot = new \App\Robot\Robot($_POST['url']);
+    $result = $robot->check()->getResults();
+    ksort($result);
+}
 
 require_once ('main.php');
 
--- app/public/php/loader/Loader.php
+++ app/public/php/loader/Loader.php
@@ -2,6 +2,16 @@
 
 class Loader
 {
+    static $directories = ['app', 'config'];
 
+    public static function load($file)
+    {
+        $scriptDir = str_replace('loader', '', __DIR__);
+        foreach (self::$directories as $dir) {
+            if (file_exists("$scriptDir$dir/$file")) {
+                require_once("$scriptDir$dir/$file");
+            }
+        }
+    }
 
 }
