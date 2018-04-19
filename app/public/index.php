aa98af3cc3f2a1ae917ca7c16d1a8753d1f83ce7
MacBook <alonasoroka1993@gmail.com>
2018-04-19 23:04:35 +0300

    table

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,5 @@
 <?php
-require './vendor/autoload.php';
+require '../../vendor/autoload.php';
 require_once ('./php/loader/Loader.php');
 Loader::load('app.php');
 Loader::load('url/UrlInterface.php');
@@ -9,11 +9,17 @@ Loader::load('robot/aRobotForUrl.php');
 Loader::load('robot/RobotForUrl.php');
 Loader::load('robot/iRobot.php');
 Loader::load('robot/Robot.php');
+Loader::load('table/table.php');
+
 
 $result=[];
 if(array_key_exists('url', $_POST)){
     $robot = new \App\Robot\Robot($_POST['url']);
     $result = $robot->check()->getResults();
+
+    $table = new \App\Table\Table();
+    $table->setResult($result);
+    $table->createTable();
     ksort($result);
 }
 
---
+++ app/public/php/app/table/Table.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Table;
+
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+    use PhpOffice\PhpSpreadsheet\Writer\IWriter;
+
+    class table
+    {
+        protected $result;
+
+        public function setResult($result)
+        {
+            $this->result = $result;
+        }
+
+        public function createTable()
+        {
+            $table = new Spreadsheet();
+            $page = $table->getActiveSheet();
+            $page->setCellValue('A1', '№');
+            $page->setCellValue('B1', 'Название проверки');
+            $page->setCellValue('C1', 'Статус');
+            $page->setCellValue('E1', 'Текущее состояние');
+
+            $i = 3;
+            foreach ($this->result as $key => $row) {
+                $page->setCellValue("A{$i}", $key);
+                $page->setCellValue("B{$i}", $row['test']);
+                $page->setCellValue("C{$i}", $row['status']);
+                if ($row['status'] === 'Оk'){
+                    $page->getStyle("C{$i}")->getFill()
+                        ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                        ->getStartColor()->setARGB('FFFF0000');
+                } else {
+                    $page->getStyle("C{$i}")->getFill()
+                        ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                        ->getStartColor()->setARGB('FF00FF00');;
+                }
+                $page->setCellValue("D{$i}", 'Состояние');
+                $page->setCellValue("E{$i}", $row['state']);
+                $i++;
+                $page->setCellValue("D{$i}", 'Рекомендации');
+                $page->setCellValue("E{$i}", $row['recommendation']);
+                $i++;
+            }
+
+            $writer = new Xlsx($table);
+            $name = 'robotsTest'.rand(1,1212).'.xlsx';
+            $writer->save($name);
+        }
+
+    }
+
